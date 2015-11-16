25029869f6a7a769742b369b889a749965e8a8b1
hewr1993 <hewr2010@gmail.com>
2015-11-16 16:25:06 +0000

    graph support

--- main.py
+++ main.py
@@ -7,48 +7,59 @@ __author__ = "Wayne Ho"
 
 import time
 import serial
-#import matplotlib.pyplot as plt
+import json
 
 
 class A4Signal(object):
     def __init__(self, signal):
+        self.legal = False
         s = [ord(x) for x in signal]
-        assert s[0] == 50 and s[1] == 61
+        if not (s[0] == 50 and s[1] == 61):
+            print "Auth failed"
+            return
         self.pm25 = s[6] * 256 + s[7]
         self.pm10 = s[8] * 256 + s[9]
         verify_sum = sum(s[:-2])
         verify_code = s[-2] * 256 + s[-1]
         if verify_sum != verify_code:
-		print "Auth failed"
+            print "Auth failed"
+            return
+        self.legal = True
 
 
 class A4Serial(object):
-    def __init__(self, limit=20, level=150):
+    def __init__(self, limit=1000, stride=1):
         self.limit = limit
-        self.level = level
+        self.stride = stride
+        self.current = -1
         self.serial = []
-#        plt.ion()
-#        plt.show()
 
     def add(self, signal):
-        self.serial.append(signal)
-        self.serial = self.serial[:self.limit]
-        v25 = [a4.pm25 for a4 in self.serial]
-        v10 = [a4.pm10 for a4 in self.serial]
-        max_val = max(v25 + v10)
-#        plt.clf()
-#        plt.xlim(0, self.limit)
-#        plt.ylim(0, max(self.level, max_val))
-#        plt.plot(v25, label="pm2.5")
-#        plt.legend()
-#        plt.plot(v10, label="pm10")
-#        plt.legend()
-#        plt.draw()
+        self.current = (self.current + 1) % self.stride
+        if self.current == 0:
+            self.serial.append(signal)
+            self.serial = self.serial[:self.limit]
+            self.export()
+
+    def content(self):
+        pm25 = [s.pm25 for s in self.serial]
+        pm10 = [s.pm10 for s in self.serial]
+        return (pm25, pm10)
+
+    def export(self):
+        json.dump(self.content(), open("data/%d.json" % self.stride, "w"))
 
 if __name__ == "__main__":
-    serial_recorder = A4Serial()
+    recs = [
+        A4Serial(stride=1),
+        A4Serial(stride=60),
+        A4Serial(stride=3600),
+    ]
     ser = serial.Serial("/dev/ttyUSB0", 9600)
     while True:
         a4 = A4Signal(ser.read(32))
-        print "[PM2.5] %d\t[PM10] %d" % (a4.pm25, a4.pm10)
-        serial_recorder.add(a4)
+        if a4.legal:
+            print "[PM2.5] %d\t[PM10] %d" % (a4.pm25, a4.pm10)
+            for rec in recs:
+                rec.add(a4)
+
